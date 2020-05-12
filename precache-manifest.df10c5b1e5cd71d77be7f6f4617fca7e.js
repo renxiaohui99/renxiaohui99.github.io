1b48dd2f8a94e1b785d7bc797ce42b817e9678e1
renxiaohui99 <572188360@qq.com>
2020-05-12 12:40:40 +0800

    Updates

---
+++ precache-manifest.df10c5b1e5cd71d77be7f6f4617fca7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0361017d11dec8cc48fe7e27536783e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98c56951f0bde6d6f7b9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "258f0316654d2be9ef88",
+    "url": "/static/js/2.b1b0848a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1b0848a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98c56951f0bde6d6f7b9",
+    "url": "/static/js/main.14ab5d48.chunk.js"
+  },
+  {
+    "revision": "38d122660160e028dd32",
+    "url": "/static/js/runtime-main.23ae2981.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
